c33d44d2da346ca2b752a8959a148969bfa48c5e
James Tan <45958726+SolidJam@users.noreply.github.com>
2022-01-25 09:58:51 +0800

    Add files via upload

---
+++ Q2B.java
@@ -0,0 +1,22 @@
+public class Q2B
+{
+    public static void main(String[] args) {
+        String currency = "CSC1009";
+
+        switch(currency)
+        {
+        case "AUD":
+            System.out.println("Australian Dollar");
+        break;
+        case "MYR":
+            System.out.println("Malaysian Dollar");
+            break;
+        case "CSC1009":
+            System.out.println("Object-Oriented Programming");
+            break;
+        case "SGD":
+            System.out.println("Singapore Dollar");
+            break;
+        }
+    }
+}
---
+++ Q3.java
@@ -0,0 +1,11 @@
+public class Q3 {
+    public static void main(String[] args) {
+        for(int x = 102; x > 66; x = x - 1)
+        {
+            if (x % 2 != 0)
+            {
+                System.out.println("Values of x:" + x);
+            }
+        }
+    }
+}
